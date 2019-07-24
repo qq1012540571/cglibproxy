180a2860b56e5e7b3bd255ef5851dbb689767f2d
huanghaoqiang <1012540571@qq.com>
2019-07-24 13:51:50 +0800

    多种方式实现类的方法实现

--- src/main/java/ProxyTeest.java
+++ src/main/java/ProxyTeest.java
@@ -1,11 +1,26 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * @author 黄豪强
  * @create 2019/7/24 9:09
  */
 public class ProxyTeest {
-    public static void main(String[] args) {
-           CglibProxy cglibProxy=new CglibProxy();
-            PlayGame playGame= (PlayGame) cglibProxy.newInstall(new PlayGame());
-            playGame.play();
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        CglibProxy cglibProx = new CglibProxy();
+        PlayGame playGame = (PlayGame) cglibProx.newInstall(new PlayGame());
+        playGame.play();
+        /**
+         * public方法的反射实现方法
+         Method method = cglibProxy.getMethod("newInstall", new Class[]{Object.class});
+         method.setAccessible(true);
+         PlayGame playGame = (PlayGame) method.invoke(cglibProx, new PlayGame());
+         playGame.play();
+         *private方法的反射实现方法
+         Method method = cglibProxy.getDeclaredMethod("newInstall", new Class[]{Object.class});
+         method.setAccessible(true);
+         PlayGame playGame = (PlayGame) method.invoke(cglibProx, new PlayGame());
+         playGame.play();
+         */
     }
 }
